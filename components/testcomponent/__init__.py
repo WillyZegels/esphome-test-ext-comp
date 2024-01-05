ced4b3cf1f18aa524b9e1ae0f21a74e0172ddb72
WillyZegels <39401643+WillyZegels@users.noreply.github.com>
2024-01-05 14:30:44 +0100

    Update __init__.py

--- components/testcomponent/__init__.py
+++ components/testcomponent/__init__.py
@@ -1,30 +1,23 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate, uart
+from esphome.components import binary_sensor
 from esphome.const import (
-    CONF_ID
-    CONF_UART_ID
-    CONF_RECEIVE_TIMEOUT
-    
-from esphome.const import (
-    CONF_ID, 
-    CONF_TRIGGER_ID
+    DEVICE_CLASS_CONNECTIVITY,
+    ENTITY_CATEGORY_DIAGNOSTIC,
 )
 
-DEPENDENCIES = {'uart'}
-CVsolarDrivec_ns = cg.esphome_ns.namespace('CVsolarDrive')
-CVsolarDriveC = CVsolarDrivec_ns.class_('CVsolarDriveC', uart.UARTDevice, cg.Component)
+status_ns = cg.esphome_ns.namespace("status")
+StatusBinarySensor = status_ns.class_(
+    "StatusBinarySensor", binary_sensor.BinarySensor, cg.Component
+)
 
-CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
-{
-    cv.GenerateID(): cv.declare_id(CVsolarDriveC),
-    cv.required(CONF_LAMBDA): cv.returning_lambda,
-    cv.required(CONF_CLIMATES): cv.ensure_list(climate.CLIMATE_SCHEMA),
-})
-    .extend(cv.polling_component_schema("10s"))
+CONFIG_SCHEMA = binary_sensor.binary_sensor_schema(
+    StatusBinarySensor,
+    device_class=DEVICE_CLASS_CONNECTIVITY,
+    entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
-    uart_component = await cg.get_variable(config[CONF_UART_ID])
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config
-    
+    var = await binary_sensor.new_binary_sensor(config)
+    await cg.register_component(var, config)
